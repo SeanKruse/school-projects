491b5bba0fc9d58d63255c74bdec53c88fb67dca
Sean Kruse <106112943+SeanKruse@users.noreply.github.com>
2022-05-23 20:01:18 -0600

    Update student.py
    Update author name

--- Principles of Database Systems/Project3/student.py
+++ Principles of Database Systems/Project3/student.py
@@ -1,7 +1,7 @@
 '''
 CS3810 - Principles of Database Systems - Spring 2022
 Instructor: Thyago Mota
-Student Names:
+Student Names: Sean Kruse
 Description: creates a Student entity with a 1-many mapping to StudentInterest and allows listing of all students
 '''
 
