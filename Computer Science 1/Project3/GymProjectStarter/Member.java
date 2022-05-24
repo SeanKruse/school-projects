9cf1371e1c579d5b1ac8fe89eadc3d208b7a919d
xDazed <sean.kruse1@gmail.com>
2022-05-23 18:57:11 -0600

    Updating Local to Remote Repos

---
+++ Computer Science 1/Project3/GymProjectStarter/Member.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class Member - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Member
+{
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
